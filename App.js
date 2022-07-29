add23a609608939b3da3ec5ddf78779a3e618632
SAnschutz <23403170+SAnschutz@users.noreply.github.com>
2022-07-29 12:33:00 -0400

    init commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -18,3 +18,35 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import AllPlaces from './screens/AllPlaces';
+// import AddPlace from './screens/AddPlace';
+
+// const Stack = createNativeStackNavigator();
+
+// export default function App() {
+//   return (
+//     <View>
+//       <StatusBar style='dark' />
+//       <NavigationContainer>
+//         <Stack.Navigation>
+//           <Stack.Screen name='AllPlaces' component={AllPlaces} />
+//           <Stack.Screen name='AddPlace' component={AddPlace} />
+//         </Stack.Navigation>
+//       </NavigationContainer>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ components/Places/PlaceForm.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+function PlaceForm() {
+  return (
+    <View>
+      <Text>The Place Form</Text>
+    </View>
+  );
+}
+
+export default PlaceForm;
+
+const styles = StyleSheet.create({});
